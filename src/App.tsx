589dfc49ea0f62c6cdfd346f472d4e3f7151061a
NayabMaryam <nayabmaryam40@gmail.com>
2026-08-04 22:25:11 +0500

    docs: add initial project documentation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>React App</h1>
+      <p>Vite + React + TypeScript</p>
+    </main>
+  )
+}
+
+export default App
