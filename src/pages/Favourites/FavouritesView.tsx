19c31a3a59d92bc3c3cb5df0a3a57a60cbad64fa
Software Engineering <baoalana123@gmail.com>
2026-08-05 23:43:11 +0500

    feat: Create FavouritesView component for favourites page

---
+++ src/pages/Favourites/FavouritesView.tsx
@@ -0,0 +1,12 @@
+// Favourites View
+// Will later render the Favourites page user interface.
+
+const FavouritesView = () => {
+  return (
+    <div>
+      {/* Favourites page content will go here */}
+    </div>
+  );
+};
+
+export default FavouritesView;
