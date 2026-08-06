c92372f042e203184b767088ec9f256321562f9a
Software Engineering <baoalana123@gmail.com>
2026-08-06 21:58:18 +0500

    chores: Implement temporary test button for Open Library API
    Added a temporary test button to trigger Open Library API search.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
+import { searchBooks } from './services/openLibraryService'
 
 function App() {
+  // TEMPORARY: manual test trigger for the Open Library API integration.
+  // Remove this once the real UI (search bar, ViewModel, etc.) is wired up.
+  const handleTestSearch = () => {
+    searchBooks('harry potter')
+      .then((books) => {
+        console.log('[App] TEMP test - books returned:', books)
+      })
+      .catch((error) => {
+        console.log('[App] TEMP test - error:', error)
+      })
+  }
+
   return (
     <BrowserRouter>
       <Header />
+
+      {/* TEMPORARY TEST BUTTON - remove after confirming API works */}
+      <button onClick={handleTestSearch}>Test Open Library API</button>
     </BrowserRouter>
   )
 }
