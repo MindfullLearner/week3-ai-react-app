8e2135f7ac4f77a4a4c6b7557fff5f523294ed1b
Software Engineering <baoalana123@gmail.com>
2026-08-07 23:55:48 +0500

    chores: refactor Header import and update props in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
+import Header from './components/Header/Header'
 import HomeView from './pages/Home/HomeView'
 import FavouritesView from './pages/Favourites/FavouritesView'
 import { useHomeViewModel } from './pages/Home/useHomeViewModel'
 
 function App() {
-  const { query, setQuery, books, loading, error, handleSearch } = useHomeViewModel()
+  const { query, setQuery, books, loading, error, handleSearch, loadInitialBooks } =
+    useHomeViewModel()
 
   return (
     <BrowserRouter>
-      <Header query={query} setQuery={setQuery} onSearch={handleSearch} />
+      <Header
+        query={query}
+        setQuery={setQuery}
+        onSearch={handleSearch}
+        onHomeClick={loadInitialBooks}
+      />
       <Routes>
         <Route
           path="/"
